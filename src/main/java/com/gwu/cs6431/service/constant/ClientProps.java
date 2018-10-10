7e231027ab6f10561616c459479e66b9686620fa
Jiuzhi Qi <35633477+jzhzj@users.noreply.github.com>
2018-10-10 15:21:26 -0400

    add properties

---
+++ src/main/java/com/gwu/cs6431/service/constant/ClientProps.java
@@ -0,0 +1,16 @@
+package com.gwu.cs6431.service.constant;
+
+import java.util.ResourceBundle;
+
+public class ClientProps {
+    static {
+        ResourceBundle rb = ResourceBundle.getBundle("client");
+        SERVER_ADDRESS = rb.getString("SERVER_ADDRESS");
+        SERVER_PORT = Integer.parseInt(rb.getString("SERVER_PORT"));
+        CLIENT_PORT = Integer.parseInt(rb.getString("CLIENT_PORT"));
+    }
+
+    public static final String SERVER_ADDRESS;
+    public static final int SERVER_PORT;
+    public static final int CLIENT_PORT;
+}
